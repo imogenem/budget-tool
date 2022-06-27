7eba9aea29365c93f5eb3be14a06639bde41804b
Varneega <varneega.theva10@gmzil.com>
2022-06-26 17:50:31 -0700

    Update car input form

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 
 import LoanInputsForm from "./components/LoanInputsForm";
 import NavBar from "./components/NavBar";
+import CarInputsForm from "./components/CarInputsForm"
 
 function App() {
   return (
     <div className="App">
         <NavBar />
         <LoanInputsForm />
+        <CarInputsForm />
     </div>
   );
 }
