0c2806834c80708ee0625873d26757c2ca760b2b
Imogene McDonald <61811702+imogenem@users.noreply.github.com>
2022-06-26 19:38:31 -0700

    Homepage (#7)
    * added initial version of custom inputs form
    
    * Update car input form
    
    * add homepage
    
    Co-authored-by: Minting Fu <mminting@student.ubc.ca>
    Co-authored-by: Varneega <varneega.theva10@gmzil.com>

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
 import React from 'react';
 
 import './App.css';
+import { CustomInputsForm } from './components/CustomInputsForm';
 
 import LoanInputsForm from "./components/LoanInputsForm";
 import NavBar from "./components/NavBar";
 import OutputTable from "./components/OutputTable";
+import CarInputsForm from "./components/CarInputsForm"
 
 function App() {
   return (
-    <div className="App">
-        <NavBar />
+    <div className="App"> 
+      <NavBar />
+      <div className="grid-container">
+        <div className="grid-item">
+        <h1>Inputs</h1>
         <LoanInputsForm />
-        <OutputTable />
+        <CarInputsForm />
+        <CustomInputsForm />
+        </div>
+        <div className="grid-item">
+        <h1>Outputs</h1>
+        <OutputTable /> 
+        </div>
+
+      </div>
     </div>
   );
 }
 
 export default App;
+
